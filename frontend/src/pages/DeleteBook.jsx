c4c143d7bd9900f50b03eaa4905cf7f801eca4d2
khadidja2005 <mk_brakta@esi.dz>
2023-10-02 10:28:20 +0200

    edit and delete book done

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import BackButtom from '../components/BackButtom'
+import Spinner from '../components/Spinner'
 
 const DeleteBook = () => {
+  const [loading , setloading] = useState(false)
+  const navigate = useNavigate()
+  const {id} = useParams()
+  const handelDeleteBook= (()=> {
+    setloading(true)
+    axios
+    .delete(`http://localhost:5555/Books/${id}`)
+    .then(()=> {
+     setloading(false)
+     navigate("/")
+    })
+    .catch((err)=> {
+      setloading(false)
+      alert("bla bla bla")
+      console.log(err)
+    })
+
+  })
+
+
   return (
-    <div>DeleteBook</div>
+    <div className='p-4 '>
+      <BackButtom/>
+      <h1 className='text-3xl my-4 '>Delete Book</h1>
+       {loading ? <Spinner/> : ""} 
+      <div className='flex flex-col border-sky-500 items-center justify-center rounded-xl w-[600px] border-2 p-8'>
+        <h3 className='text-2xl'> Are you sure to delete this book</h3>
+        <button 
+        className=' bg-red-600 p-2 text-white my-4 ' 
+        onClick={handelDeleteBook}> yes i am sure</button>
+
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -9,28 +9,29 @@ const EditBook = () => {
   const [author, setauthor] = useState("");
   const [publishYear, setpublishYear] = useState("");
   const [loading, setloading] = useState(false);
-  const {id} = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
-    useEffect(()=> {  
-          axios.get(`http://localhost:5555/Books/${id}`)
-      .then((res)=> {
-        settitle(res.data.title)
-        setauthor(res.data.author)
-        setpublishYear(res.data.publishYear)
-        setloading(false)
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5555/Books/${id}`)
+      .then((res) => {
+        settitle(res.data.title);
+        setauthor(res.data.author);
+        setpublishYear(res.data.publishYear);
+        setloading(false);
       })
-      .catch((err)=> {
-        setloading(false) 
-        console.log(err)
-      })
-    },[])
-    const handelEditbook = () => {
-      const data = {
-        title,
-        author,
-        publishYear,
-        loading,
-      };
+      .catch((err) => {
+        setloading(false);
+        console.log(err);
+      });
+  }, []);
+  const handelEditbook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+      loading,
+    };
     axios
       .put(`http://localhost:5555/Books/${id}`, data)
       .then(() => {
@@ -51,42 +52,48 @@ const EditBook = () => {
       <div className="flex flex-col border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
-          <input 
-          type="text" 
-          name="" 
-          id=""
-          value={title}
-          onChange={(e)=>{settitle(e.target.value)}} 
-          className="border-2 border-gray-500 px-4 py-2 w-full"
-          /> 
+          <input
+            type="text"
+            name=""
+            id=""
+            value={title}
+            onChange={(e) => {
+              settitle(e.target.value);
+            }}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
         </div>
 
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Author</label>
-          <input 
-          type="text" 
-          name="" 
-          id=""
-          value={author}
-          onChange={(e)=>{setauthor(e.target.value)}} 
-          className="border-2 border-gray-500 px-4 py-2 w-full"
-          /> 
+          <input
+            type="text"
+            name=""
+            id=""
+            value={author}
+            onChange={(e) => {
+              setauthor(e.target.value);
+            }}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
         </div>
 
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">publishYear</label>
-          <input 
-          type="text" 
-          name="" 
-          id=""
-          value={publishYear}
-          onChange={(e)=>{setpublishYear(e.target.value)}} 
-          className="border-2 border-gray-500 px-4 py-2 w-full"
-          /> 
+          <input
+            type="text"
+            name=""
+            id=""
+            value={publishYear}
+            onChange={(e) => {
+              setpublishYear(e.target.value);
+            }}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
         </div>
 
         <button className="p-2 bg-sky-300 m-8" onClick={handelEditbook}>
-           Save
+          Save
         </button>
       </div>
     </div>
